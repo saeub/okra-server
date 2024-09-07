3553a600dad0ab8fce462f920e327614ae9ff05e
Andreas Säuberli <andreas.saeuberli@gmail.com>
2024-09-07 09:34:08 +0200

    Configure database host and name with environment variables

--- web/okra_server/settings_prod.py
+++ web/okra_server/settings_prod.py
@@ -9,8 +9,8 @@ DEBUG = False
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": "postgres",
-        "NAME": "okra_server",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
